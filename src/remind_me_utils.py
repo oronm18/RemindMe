7009278bc14305638dbe9d15d29884c6ff960043
oronm18 <oronm18@gmail.com>
2023-10-22 11:30:44 +0300

    Utils for the project

---
+++ src/remind_me_utils.py
@@ -0,0 +1,50 @@
+"""
+Utils file for the project
+Author: Oron Moshe
+Date: 22/10/2023
+"""
+# ----- Imports ----- #
+import dataclasses
+import json
+import uuid
+import bcrypt
+
+
+# ----- Functions ----- #
+
+def generate_unique_id() -> str:
+    """
+    Generate a unique id for an item.
+    :return: A new unique id.
+    """
+    return str(uuid.uuid4())
+
+
+def compare_hashes(hash1: str, hash2: str) -> bool:
+    """
+    Compare 2 given hashes.
+    :param hash1: First given hash.
+    :param hash2: Second given hash.
+    :return: Does the hashes equals?
+    """
+    return bcrypt.checkpw(hash1.encode('utf-8'), hash2.encode('utf-8'))
+
+
+def hash_password(password: str) -> str:
+    """
+    Hash a password.
+    :param password: Given password.
+    :return: The hashed input.
+    """
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
+
+# ----- Classes ----- #
+
+
+class DataclassJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        return super().default(obj)

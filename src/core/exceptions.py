56946ab5d1d490a5702d741a949f9025fdcbf759
oronm18 <oronm18@gmail.com>
2023-10-22 11:30:34 +0300

    Adding exxeptions

---
+++ src/core/exceptions.py
@@ -0,0 +1,14 @@
+"""
+Abstract exception.
+Author: Oron Moshe
+Date: 22/10/2023
+"""
+
+# ----- Exceptions ----- #
+
+
+class RemindMeBaseException(Exception):
+    """
+    Abstract exception for the project.
+    """
+    pass

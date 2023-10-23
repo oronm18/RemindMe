6034e0ebd19a8744da08c27ac3f90c2d3fe2d3a8
oronm18 <oronm18@gmail.com>
2023-10-23 09:36:54 +0300

    Event and User data classes

--- src/core/event.py
+++ src/core/event.py
@@ -6,72 +6,84 @@ Date: 22/10/2023
 # ----- Imports ----- #
 
 import dataclasses
-from remind_me_utils import generate_unique_id
+from typing import Optional
+
+from core.exceptions import RemindMeBaseException
 from datetime import datetime
+
+# ----- Exceptions ----- #
+
+
+class EventAlreadyExist(RemindMeBaseException):
+    """
+    Event already exist exception.
+    """
+    pass
+
+
+class EventDoesNotExist(RemindMeBaseException):
+    """
+    Event does not exist exception.
+    """
+    pass
+
+
+class EventAlreadyInEvent(RemindMeBaseException):
+    """
+    Event already in the event exception.
+    """
+    pass
+
+
+class EventDoesNotInEvent(RemindMeBaseException):
+    """
+    Event does not in the event exception.
+    """
+    pass
+
+
+class ModifyChangesAreInvalid(RemindMeBaseException):
+    """
+    Modify changes are invalid exception.
+    """
+    pass
+
+
+class InvalidAttribute(RemindMeBaseException):
+    """
+    Invalid attribute exception.
+    """
+    pass
+
+
+class UserAlreadySubscriber(RemindMeBaseException):
+    """
+    User is already a subscriber exception.
+    """
+    pass
+
+
+class UserDoesNotASubscriber(RemindMeBaseException):
+    """
+    User is not a subscriber exception.
+    """
+    pass
 
 
 # ----- Classes ----- #
 
 
 @dataclasses.dataclass
-class EventDetails:
+class Event:
     """
     Dataclass that store events details.
     """
-    event_id: str
+    event_id: Optional[str]
     created_user_id: str
     event_name: str
     event_description: str
+    location: str
+    subscribers: list[str]  # list of users ids.
     event_start_time: datetime
     event_end_time: datetime
-
-
-class BaseEvent:
-    """
-    Base Event for the project.
-    """
-    def __init__(
-            self,
-            event_name: str,
-            created_user_id: str,
-            event_description: str,
-            event_start_time: datetime,
-            event_end_time: datetime = None
-    ):
-        """
-        Initialize a new event.
-        :param event_name: Event name.
-        :param event_description: Description of the event.
-        :param event_start_time: When does it start.
-        :param event_end_time: When does it end.
-        """
-        # Make the end time be the start time if not given.
-        event_end_time = event_end_time if event_end_time else event_start_time
-
-        self.event_details = EventDetails(
-            event_id=generate_unique_id(),
-            event_name=event_name,
-            created_user_id=created_user_id,
-            event_description=event_description,
-            event_start_time=event_start_time,
-            event_end_time=event_end_time
-        )
-
-    def set_event_details(
-            self,
-            event_name: str = None,
-            event_description: str = None,
-            event_start_time: datetime = None,
-            event_end_time: datetime = None
-    ):
-        if event_name:
-            self.event_details.event_name = event_name
-        if event_description:
-            self.event_details.event_description = event_description
-        if event_start_time:
-            self.event_details.event_start_time = event_start_time
-        if event_end_time:
-            self.event_details.event_end_time = event_end_time
-
-
-# ----- Functions ----- #
+    creation_time: Optional[datetime]
--- src/core/user.py
+++ src/core/user.py
@@ -6,10 +6,9 @@ Date: 22/10/2023
 # ----- Imports ----- #
 
 import dataclasses
+from typing import Optional
 
 from core.exceptions import RemindMeBaseException
-from remind_me_utils import generate_unique_id
-from datetime import datetime
 
 
 # ----- Exceptions ----- #
@@ -50,27 +49,8 @@ class User:
     """
     Dataclass that store events details.
     """
-    user_id: str
+    user_id: Optional[str]
     user_name: str
     user_mail: str
     hashed_password: str
-    events: list[str] = dataclasses.field(default_factory=list)
-    #
-    # def add_event(self, event_id):
-    #     """
-    #     Add an event id to the user events.
-    #     :param event_id: A given event id.
-    #     :return: None
-    #     """
-    #     if event_id in self.events:
-    #         raise UserAlreadyExist()
-    #     self.events.append(event_id)
-    #
-    # def remove_event(self, event_id):
-    #     """
-    #     Remove an event id from the user events.
-    #     :param event_id: A given event id.
-    #     :return: None
-    #     """
-    #     self.events = [event for event in self.events if event != event_id]
-    #
+    hosts_events: list[str] = dataclasses.field(default_factory=list)
